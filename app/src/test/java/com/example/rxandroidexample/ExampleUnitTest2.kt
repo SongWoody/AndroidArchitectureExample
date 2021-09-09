713a8b70ab8546b6704018f8bbf18e04d2aaeca5
Woody <syg1138@gmail.com>
2021-09-09 23:55:56 +0900

    rx example - range, timer

--- app/src/test/java/com/example/rxandroidexample/ExampleUnitTest2.kt
+++ app/src/test/java/com/example/rxandroidexample/ExampleUnitTest2.kt
@@ -2,6 +2,7 @@ package com.example.rxandroidexample
 
 import io.reactivex.rxjava3.core.Observable
 import org.junit.Test
+import java.util.concurrent.TimeUnit
 
 // rx operation
 class ExampleUnitTest2 {
@@ -24,5 +25,41 @@ class ExampleUnitTest2 {
         deferSrc.subscribe {
             println("#2 deferSrc $it")
         }
+    }
+
+    @Test
+    fun emptyAndNever() {
+        Observable.empty<String>().doOnTerminate {
+            println("empty HI")
+        }.subscribe {
+            println("#1 it")
+        }
+
+        Observable.never<String>().doOnTerminate {
+            println("never HI")
+        }.subscribe {
+            println("#1 it")
+        }
+
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun range() {
+        Observable.range(1,10).subscribe {
+            println("#$it")
+        }
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun timer() {
+        val src = Observable.timer(1, TimeUnit.SECONDS)
+        println("구독")
+        src.subscribe {
+            println("실행")
+        }
+        println("Sleep")
+        Thread.sleep(1000)
     }
 }
