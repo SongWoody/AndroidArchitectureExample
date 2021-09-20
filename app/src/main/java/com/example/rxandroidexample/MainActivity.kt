d94fccfa91470aafb3406c954081279d77f49d66
Woody <syg1138@gmail.com>
2021-09-20 17:48:41 +0900

    room example - print item

--- app/src/main/java/com/example/rxandroidexample/MainActivity.kt
+++ app/src/main/java/com/example/rxandroidexample/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.rxandroidexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.room.Room
+import com.example.rxandroidexample.databinding.ActivityMainBinding
+import com.example.rxandroidexample.room.Todo
+import com.example.rxandroidexample.room.TodoDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var dataBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val newTodo = Todo("test","test todo")
+        val db = Room.databaseBuilder(
+            applicationContext,
+            TodoDatabase::class.java,
+            "todo-database"
+        ).build()
+
+        dataBinding.addButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                db.todoDao().insert(newTodo)
+            }
+        }
+
+        dataBinding.selectButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val todoList = db.todoDao().getAllTodoList()
+                todoList.map { "id: ${it.id} title: ${it.title} " }.forEach {
+                    dataBinding.test.append("$it ,")
+                }
+
+            }
+        }
+
+
+
     }
 }
