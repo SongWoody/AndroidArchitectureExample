b015bcb6ee168fd2a5a5251e20a173d9173ef01e
Woody <syg1138@gmail.com>
2021-12-15 23:52:58 +0900

    [Compose] 기본 설정 및 연습 화면 추가

---
+++ app/src/main/java/com/example/rxandroidexample/scene/ComposeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.rxandroidexample.scene
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
+
+class ComposeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
