28816ebf78457c4bc80d902a213c1380efdcfe06
Woody <syg1138@gmail.com>
2021-12-22 01:46:38 +0900

    [Compose] Row 추가

--- app/src/main/java/com/example/rxandroidexample/scene/ComposeActivity.kt
+++ app/src/main/java/com/example/rxandroidexample/scene/ComposeActivity.kt
@@ -3,10 +3,15 @@ package com.example.rxandroidexample.scene
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.rxandroidexample.R
 
 class ComposeActivity: AppCompatActivity() {
 
@@ -20,9 +25,15 @@ class ComposeActivity: AppCompatActivity() {
 
 @Composable
 private fun Greeting(title: String) {
-    Column() {
-        Text(text = title)
-        Text(text = "hi")
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_baseline_add_24),
+            contentDescription = ""
+        )
+        Column() {
+            Text(text = title)
+            Text(text = "hi")
+        }
     }
 }
 
