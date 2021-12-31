fad3aa6e4cc15aef4cdee6edb7609bccf4031c7c
Woody <syg1138@gmail.com>
2021-12-31 12:54:45 +0900

    [Compose] Surface animation

--- app/src/main/java/com/example/rxandroidexample/scene/ComposeActivity.kt
+++ app/src/main/java/com/example/rxandroidexample/scene/ComposeActivity.kt
@@ -4,6 +4,8 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -11,11 +13,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -69,6 +73,9 @@ private fun MessageCard(msg: Message) {
         }
         Spacer(modifier = Modifier.width(8.dp))
         var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            targetValue = if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
         Column(modifier = Modifier.clickable {
             isExpanded = !isExpanded
         }) {
@@ -77,11 +84,18 @@ private fun MessageCard(msg: Message) {
                 color = MaterialTheme.colors.secondaryVariant
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = msg.body,
-                maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                style = MaterialTheme.typography.body2
-            )
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
