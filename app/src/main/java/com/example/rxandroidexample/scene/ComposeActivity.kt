2a82710da5233340cecf4bb738c6dc12f0599223
Woody <syg1138@gmail.com>
2022-01-03 23:41:06 +0900

    [Compose] OutlineTextField 사용

--- app/src/main/java/com/example/rxandroidexample/scene/ComposeActivity.kt
+++ app/src/main/java/com/example/rxandroidexample/scene/ComposeActivity.kt
@@ -13,10 +13,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Checkbox
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -101,6 +98,9 @@ private fun MessageCard(msg: Message) {
                     style = MaterialTheme.typography.body2
                 )
             }
+            OutlinedTextField(value = "Outlinke", onValueChange = {
+
+            })
         }
         var isSelected by remember {
             mutableStateOf(false)
