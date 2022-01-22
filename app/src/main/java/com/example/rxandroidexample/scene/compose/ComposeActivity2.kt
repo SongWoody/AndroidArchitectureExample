fa21a3d91e17ad1df99f0dcb629475c66aa1745f
Woody <syg1138@gmail.com>
2022-01-22 22:45:09 +0900

    [Compose] Compose Card

--- app/src/main/java/com/example/rxandroidexample/scene/compose/ComposeActivity2.kt
+++ app/src/main/java/com/example/rxandroidexample/scene/compose/ComposeActivity2.kt
@@ -3,8 +3,13 @@ package com.example.rxandroidexample.scene.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class ComposeActivity2: ComponentActivity() {
 
@@ -20,5 +25,14 @@ class ComposeActivity2: ComponentActivity() {
 
 @Composable
 private fun ComposeView() {
-    Text(text = "Hi ComposeActivity2")
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+            .clickable {  },
+        elevation = 10.dp
+    ) {
+        Text(text = "Hi Card View"
+        , modifier = Modifier.padding(15.dp))
+    }
 }
