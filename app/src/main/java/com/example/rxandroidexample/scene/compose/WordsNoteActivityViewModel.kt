881f0771c82901cbca98d0757934d78d07e0952e
Woody <syg1138@gmail.com>
2022-02-20 23:59:06 +0900

    [Compose]

--- app/src/main/java/com/example/rxandroidexample/scene/compose/WordsNoteActivityViewModel.kt
+++ app/src/main/java/com/example/rxandroidexample/scene/compose/WordsNoteActivityViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import com.example.rxandroidexample.scene.compose.data.Word
 
 class WordsNoteActivityViewModel: ViewModel() {
+    //todo: 디비로 데이터 관리
     val words = MutableLiveData(listOf(
         Word("Compose", "구성하다"),
         Word("Dentist", "치과 의사")
