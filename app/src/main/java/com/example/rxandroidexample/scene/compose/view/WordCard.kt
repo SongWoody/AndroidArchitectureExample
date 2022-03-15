0b04cb46f28e7302a9939d41e56c0084f9a192f4
Woody <syg1138@gmail.com>
2022-03-15 23:29:29 +0900

    [Compose] Card 디자인 변경

--- app/src/main/java/com/example/rxandroidexample/scene/compose/view/WordCard.kt
+++ app/src/main/java/com/example/rxandroidexample/scene/compose/view/WordCard.kt
@@ -1,13 +1,12 @@
 package com.example.rxandroidexample.scene.compose.view
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.rxandroidexample.room.word.Word
@@ -20,16 +19,22 @@ fun WordCard(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
-            .padding(8.dp)
+            .padding(horizontal = 12.dp, vertical = 6.dp),
+        elevation = 7.dp
     ) {
-        Column() {
+        Column(
+            modifier = Modifier.padding(vertical = 5.dp)
+        ) {
             Text(
                 word.word,
-                modifier = Modifier.padding(start = 12.dp, top = 8.dp)
+                modifier = Modifier.padding(start = 12.dp, top = 8.dp),
+                color = Color(0xff2f2f2f)
             )
+            Spacer(modifier = Modifier.height(5.dp))
             Text(
                 word.mean,
-                modifier = Modifier.padding(start = 12.dp, bottom = 8.dp)
+                modifier = Modifier.padding(start = 12.dp, bottom = 8.dp),
+                color = Color(0xff2f2f2f)
             )
         }
     }
