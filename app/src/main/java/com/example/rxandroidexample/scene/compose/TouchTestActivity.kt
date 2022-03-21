88d2f385d7815d73b645d40d78f5ea0ce660a30f
Woody <syg1138@gmail.com>
2022-03-21 23:47:32 +0900

    [Compose] 동작 Touch Test - pointerInput Test

--- app/src/main/java/com/example/rxandroidexample/scene/compose/TouchTestActivity.kt
+++ app/src/main/java/com/example/rxandroidexample/scene/compose/TouchTestActivity.kt
@@ -1,9 +1,11 @@
 package com.example.rxandroidexample.scene.compose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.rxandroidexample.scene.compose.ui.theme.RxAndroidExampleTheme
 
@@ -38,8 +41,21 @@ fun Greeting2() {
         mutableStateOf(0)
     }
     Text(text = "Hello ${count.value}!",
-    modifier = Modifier.clickable {
-        count.value += 1
+    modifier = Modifier.pointerInput(Unit) {
+        detectTapGestures(
+            onPress = {
+                Log.i("TouchTest", "onPress")
+            },
+            onDoubleTap = {
+                Log.i("TouchTest", "onDoubleTap")
+            },
+            onLongPress = {
+                Log.i("TouchTest", "onLongPress")
+            },
+            onTap = {
+                Log.i("TouchTest", "onTap")
+            }
+        )
     })
 }
 
