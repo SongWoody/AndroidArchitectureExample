6f0eb84479aec54abaa4e0ed8f38773e6b705f90
Woody <syg1138@gmail.com>
2022-03-23 23:27:06 +0900

    [Compose] modifier verticalScroll test

--- app/src/main/java/com/example/rxandroidexample/scene/compose/TouchTestActivity.kt
+++ app/src/main/java/com/example/rxandroidexample/scene/compose/TouchTestActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,8 +20,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.rxandroidexample.scene.compose.ui.theme.RxAndroidExampleTheme
 
 class TouchTestActivity : ComponentActivity() {
@@ -40,23 +48,36 @@ fun Greeting2() {
     val count = remember {
         mutableStateOf(0)
     }
-    Text(text = "Hello ${count.value}!",
-    modifier = Modifier.pointerInput(Unit) {
-        detectTapGestures(
-            onPress = {
-                Log.i("TouchTest", "onPress")
-            },
-            onDoubleTap = {
-                Log.i("TouchTest", "onDoubleTap")
-            },
-            onLongPress = {
-                Log.i("TouchTest", "onLongPress")
-            },
-            onTap = {
-                Log.i("TouchTest", "onTap")
+    Column() {
+        Text(text = "Hello ${count.value}!",
+            modifier = Modifier.pointerInput(Unit) {
+                detectTapGestures(
+                    onPress = {
+                        Log.i("TouchTest", "onPress")
+                    },
+                    onDoubleTap = {
+                        Log.i("TouchTest", "onDoubleTap")
+                    },
+                    onLongPress = {
+                        Log.i("TouchTest", "onLongPress")
+                    },
+                    onTap = {
+                        Log.i("TouchTest", "onTap")
+                    }
+                )
+            })
+        Column(
+            modifier = Modifier
+                .background(Color.Gray)
+                .size(200.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            repeat(20) {
+                Text(text = "Item $it", modifier = Modifier.padding(3.dp))
             }
-        )
-    })
+        }
+    }
+    
 }
 
 @Preview(showBackground = true)
